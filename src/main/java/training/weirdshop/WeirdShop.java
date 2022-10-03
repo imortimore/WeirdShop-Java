3a3281f861083c5a3e93d395eaa5cf4c137043db
jmbanks <Joshua.M.Banks@aexp.com>
2022-10-03 16:13:50 +0100

    Refactored specialItems to auto-add premiums

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -9,6 +9,8 @@ class WeirdShop {
     ArrayList<String> specialItems = new ArrayList<>(Arrays.asList("Aged Brie","Backstage Pass","Gold Coin"));
     public WeirdShop(Item[] items) {
         this.items = items;
+        int itemSize = specialItems.size();
+        for(int i=0; i<itemSize;i++){specialItems.add("Premium "+specialItems.get(i));}
     }
 
     void updateQuality() {
@@ -18,7 +20,7 @@ class WeirdShop {
             }else{//adds quality if not at max for backstage & brie
                 items[i].increaseQuality();
                 //increase quality of backstage pass based on days remaining
-                if (items[i].name.equals("Backstage Pass")&&items[i].sellIn < 12) {
+                if (items[i].name.contains("Backstage Pass")&&items[i].sellIn < 12) {
                     items[i].increaseQuality();
                     if (items[i].sellIn < 7) {
                         items[i].increaseQuality();
@@ -26,14 +28,14 @@ class WeirdShop {
                 }
             }
             //reduces sell-in value of everything except gold coins
-            if (!items[i].name.equals("Gold Coin")) {items[i].sellIn = items[i].sellIn - 1;}
+            if (!items[i].name.contains("Gold Coin")) {items[i].sellIn = items[i].sellIn - 1;}
             //handle expired items
             if (items[i].sellIn<0){
                 if (!specialItems.contains(items[i].name)){
                     items[i].decreaseQuality();
-                }else if(items[i].name.equals("Aged Brie")){
+                }else if(items[i].name.contains("Aged Brie")){
                     items[i].increaseQuality();
-                } else if (items[i].name.equals("Backstage Pass")) {
+                } else if (items[i].name.contains("Backstage Pass")) {
                     items[i].quality = 0;
                 }
             }
